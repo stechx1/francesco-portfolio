dee4d57105785be35509ae3fd45c4d8a46bc7e61
techx1 <ss.uitech01@gmail.com>
2022-10-12 02:10:01 +0500

    feat: add footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,7 @@
+export const Footer = () => {
+  return (
+    <div className="text-white mt-[90px] text-center">
+      <p>© 2022 Francesco Aliatis. All Rights Reserved.</p>
+    </div>
+  );
+};
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export * from './Navbar';
 export * from './Button';
 export * from './Heading';
+export * from './Footer';
